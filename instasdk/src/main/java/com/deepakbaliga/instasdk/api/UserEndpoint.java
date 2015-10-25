cc03750b26163dbdc0e8db9d352434619293509c
deepakbaliga <deepak.baliga9@gmail.com>
2015-10-25 15:08:42 +0530

    Version 1.0

--- instasdk/src/main/java/com/deepakbaliga/instasdk/api/UserEndpoint.java
+++ instasdk/src/main/java/com/deepakbaliga/instasdk/api/UserEndpoint.java
@@ -1,6 +1,7 @@
 package com.deepakbaliga.instasdk.api;
 
 import com.deepakbaliga.instasdk.entity.reponse.FeedResponse;
+import com.deepakbaliga.instasdk.entity.reponse.UserList;
 import com.deepakbaliga.instasdk.entity.reponse.UserResponse;
 import com.deepakbaliga.instasdk.util.InstaConstants;
 
@@ -21,7 +22,7 @@ public interface UserEndpoint {
      * @param userID the ID of the user or self
      * @return Information about the user
      */
-    @GET("users/{user-id}")
+    @GET(InstaConstants.Endpoint.GET_USER)
     public Call<UserResponse> getUser(@Path(InstaConstants.USERID) String userID,
                                       @Query(InstaConstants.ACCESSTOKEN) String token);
 
@@ -32,11 +33,18 @@ public interface UserEndpoint {
      * @param token access token of authenticated user
      * @return Feeds and pagination information
      */
-    @GET("/users/self/feed")
+    @GET(InstaConstants.Endpoint.GET_FEEDS)
     public Call<FeedResponse> getFeeds(@Query(InstaConstants.ACCESSTOKEN) String token);
 
 
-    @GET("/users/self/feed")
+    /**
+     * See the authenticated user's feed.
+     *
+     * @param token access token of authenticated user
+     * @param count Count of media to return.
+     * @return Feeds and pagination information
+     */
+    @GET(InstaConstants.Endpoint.GET_FEEDS)
     public Call<FeedResponse> getFeeds(@Query(InstaConstants.COUNT) int count,
                                        @Query(InstaConstants.ACCESSTOKEN) String token);
 
@@ -45,15 +53,120 @@ public interface UserEndpoint {
      *
      * @param count Count of media to return.
      * @param minID Return media later than this min_id.
-     * @param maxID Return media earlier than this max_id.s
+     * @param maxID Return media earlier than this max_id.
      * @param token access token of authenticated user
      * @return Feeds and pagination information
      */
-    @GET("/users/self/feed")
+    @GET(InstaConstants.Endpoint.GET_FEEDS)
     public Call<FeedResponse> getFeeds(@Query(InstaConstants.COUNT) int count,
                                        @Query(InstaConstants.MIN_ID) String minID,
                                        @Query(InstaConstants.MAX_ID) String maxID,
                                        @Query(InstaConstants.ACCESSTOKEN) String token);
+
+
+    /**
+     * Get the most recent media published by a user. To get the most recent media published by
+     * the owner of the access token, you can use self instead of the user-id.
+     *
+     * @param userID the ID of the user or self
+     * @param token  access token of authenticated user
+     * @return Feeds and pagination information
+     */
+    @GET(InstaConstants.Endpoint.GET_RECENT_MEDIA)
+    public Call<FeedResponse> getRecentMedia(@Query(InstaConstants.USERID) String userID,
+                                             @Query(InstaConstants.ACCESSTOKEN) String token);
+
+
+    /**
+     * Get the most recent media published by a user. To get the most recent media published by
+     * the owner of the access token, you can use self instead of the user-id.
+     *
+     * @param userID the ID of the user or self
+     * @param count  Count of media to return.
+     * @param token  access token of authenticated user
+     * @return Feeds and pagination information
+     */
+    @GET(InstaConstants.Endpoint.GET_RECENT_MEDIA)
+    public Call<FeedResponse> getRecentMedia(@Query(InstaConstants.USERID) String userID,
+                                             @Query(InstaConstants.COUNT) int count,
+                                             @Query(InstaConstants.ACCESSTOKEN) String token);
+
+    /**
+     * Get the most recent media published by a user. To get the most recent media published by
+     * the owner of the access token, you can use self instead of the user-id.
+     *
+     * @param userID the ID of the user or self
+     * @param count  Count of media to return.
+     * @param minID  Return media later than this min_id.
+     * @param maxID  Return media earlier than this max_id.
+     * @param token  access token of authenticated user
+     * @return Feeds and pagination information
+     */
+    @GET(InstaConstants.Endpoint.GET_RECENT_MEDIA)
+    public Call<FeedResponse> getRecentMedia(@Query(InstaConstants.USERID) String userID,
+                                             @Query(InstaConstants.COUNT) int count,
+                                             @Query(InstaConstants.MIN_ID) String minID,
+                                             @Query(InstaConstants.MAX_ID) String maxID,
+                                             @Query(InstaConstants.ACCESSTOKEN) String token);
+
+
+    /**
+     * Get the most recent media published by a user. To get the most recent media published by
+     * the owner of the access token, you can use self instead of the user-id.
+     *
+     * @param userID       the ID of the user or self
+     * @param count        Count of media to return.
+     * @param minTimeStamp Return media after this UNIX timestamp.
+     * @param maxTimeStamp Return media before this UNIX timestamp.
+     * @param token        access token of authenticated user
+     * @return Feeds and pagination information
+     */
+    @GET(InstaConstants.Endpoint.GET_RECENT_MEDIA)
+    public Call<FeedResponse> getRecentMedia(@Query(InstaConstants.USERID) String userID,
+                                             @Query(InstaConstants.COUNT) int count,
+                                             @Query(InstaConstants.MIN_TIMESTAMP) long minTimeStamp,
+                                             @Query(InstaConstants.MAX_TIMESTAMP) long maxTimeStamp,
+                                             @Query(InstaConstants.ACCESSTOKEN) String token);
+
+
+    /**
+     * Get the list of media liked by the authenticated user. Private media is returned as long as
+     * the authenticated user has permission to view that media. Liked media lists are only available
+     * for the currently authenticated user.
+     *
+     * @param count     Count of media to return.
+     * @param maxLikeID Return media liked before this id.
+     * @param token     access token of authenticated user
+     * @return
+     */
+    @GET(InstaConstants.Endpoint.GET_LIKED_MEDIA)
+    public Call<FeedResponse> getLikedMedia(@Query(InstaConstants.COUNT) int count,
+                                            @Query(InstaConstants.MAX_LIKE_ID) String maxLikeID,
+                                            @Query(InstaConstants.ACCESSTOKEN) String token);
+
+    /**
+     * Search for a user by name.
+     *
+     * @param query A query string.
+     * @param token access token of authenticated user
+     * @return List of users
+     */
+    @GET(InstaConstants.Endpoint.GET_LIKED_MEDIA)
+    public Call<UserList> searchUser(@Query(InstaConstants.QUERY) String query,
+                                     @Query(InstaConstants.ACCESSTOKEN) String token);
+
+    /**
+     * Search for a user by name.
+     *
+     * @param query A query string.
+     * @param count Number of users to return.
+     * @param token access token of authenticated user
+     * @return List of users
+     */
+    @GET(InstaConstants.Endpoint.SEARCH_USER)
+    public Call<UserList> searchUser(@Query(InstaConstants.QUERY) String query,
+                                     @Query(InstaConstants.COUNT) int count,
+                                     @Query(InstaConstants.ACCESSTOKEN) String token);
 
 
 }
--- instasdk/src/main/java/com/deepakbaliga/instasdk/util/InstaConstants.java
+++ instasdk/src/main/java/com/deepakbaliga/instasdk/util/InstaConstants.java
@@ -5,11 +5,28 @@ package com.deepakbaliga.instasdk.util;
  */
 public class InstaConstants {
     public static final String NO_INTERNET_CONNECTION = "Not connected to internet";
+
     public static final String INSTA_BASE_URL = "https://api.instagram.com/v1/";
+
+
     public static final String ACCESSTOKEN = "access_token";
     public static final String USERID = "user-id";
     public static final String COUNT = "count";
     public static final String MIN_ID = "min_id";
     public static final String MAX_ID = "max_id";
+    public static final String MAX_TIMESTAMP = "max_timestamp";
+    public static final String MIN_TIMESTAMP = "min_timestamp";
+    public static final String MAX_LIKE_ID = "max_like_id";
+    public static final String QUERY = "q";
+
+    public static class Endpoint {
+
+        public static final String GET_USER = "users/{user-id}";
+        public static final String GET_FEEDS = "users/self/feed";
+        public static final String GET_RECENT_MEDIA = "users/{user-id}/media/recent";
+        public static final String GET_LIKED_MEDIA = "users/self/media/liked";
+        public static final String SEARCH_USER = "users/search";
+
+    }
 
 }
