0b715047590c02d1db53178136a691147fc51556
deepakbaliga <deepak.baliga9@gmail.com>
2015-10-25 14:36:39 +0530

    Version 1.0

--- instasdk/src/main/java/com/deepakbaliga/instasdk/api/UserEndpoint.java
+++ instasdk/src/main/java/com/deepakbaliga/instasdk/api/UserEndpoint.java
@@ -1,8 +1,59 @@
 package com.deepakbaliga.instasdk.api;
 
+import com.deepakbaliga.instasdk.entity.reponse.FeedResponse;
+import com.deepakbaliga.instasdk.entity.reponse.UserResponse;
+import com.deepakbaliga.instasdk.util.InstaConstants;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
 /**
  * Created by deezdroid on 24/10/15.
  */
 public interface UserEndpoint {
+
+    /**
+     * Get basic information about a user. To get information about the owner of
+     * the access token, you can use self instead of the user-id.
+     *
+     * @param userID the ID of the user or self
+     * @return Information about the user
+     */
+    @GET("users/{user-id}")
+    public Call<UserResponse> getUser(@Path(InstaConstants.USERID) String userID,
+                                      @Query(InstaConstants.ACCESSTOKEN) String token);
+
+
+    /**
+     * See the authenticated user's feed.
+     *
+     * @param token access token of authenticated user
+     * @return Feeds and pagination information
+     */
+    @GET("/users/self/feed")
+    public Call<FeedResponse> getFeeds(@Query(InstaConstants.ACCESSTOKEN) String token);
+
+
+    @GET("/users/self/feed")
+    public Call<FeedResponse> getFeeds(@Query(InstaConstants.COUNT) int count,
+                                       @Query(InstaConstants.ACCESSTOKEN) String token);
+
+    /**
+     * See the authenticated user's feed.
+     *
+     * @param count Count of media to return.
+     * @param minID Return media later than this min_id.
+     * @param maxID Return media earlier than this max_id.s
+     * @param token access token of authenticated user
+     * @return Feeds and pagination information
+     */
+    @GET("/users/self/feed")
+    public Call<FeedResponse> getFeeds(@Query(InstaConstants.COUNT) int count,
+                                       @Query(InstaConstants.MIN_ID) String minID,
+                                       @Query(InstaConstants.MAX_ID) String maxID,
+                                       @Query(InstaConstants.ACCESSTOKEN) String token);
+
 
 }
--- instasdk/src/main/java/com/deepakbaliga/instasdk/util/InstaConstants.java
+++ instasdk/src/main/java/com/deepakbaliga/instasdk/util/InstaConstants.java
@@ -4,7 +4,12 @@ package com.deepakbaliga.instasdk.util;
  * Created by deezdroid on 24/10/15.
  */
 public class InstaConstants {
-    public static final String ACCESSTOKEN = "accesstoken";
     public static final String NO_INTERNET_CONNECTION = "Not connected to internet";
     public static final String INSTA_BASE_URL = "https://api.instagram.com/v1/";
+    public static final String ACCESSTOKEN = "access_token";
+    public static final String USERID = "user-id";
+    public static final String COUNT = "count";
+    public static final String MIN_ID = "min_id";
+    public static final String MAX_ID = "max_id";
+
 }
