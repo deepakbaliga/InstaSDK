06a83a30e824e9f874f730107be70b76fe65d20f
deepakbaliga <deepak.baliga9@gmail.com>
2015-10-27 16:15:10 +0530

    Version 1.0

--- instasdk/src/main/java/com/deepakbaliga/instasdk/InstaMedia.java
+++ instasdk/src/main/java/com/deepakbaliga/instasdk/InstaMedia.java
@@ -1,12 +1,15 @@
 package com.deepakbaliga.instasdk;
 
 import android.content.Context;
+import android.support.annotation.IntRange;
 
 import com.deepakbaliga.instasdk.api.MediaEndpoint;
 import com.deepakbaliga.instasdk.api.UserEndpoint;
+import com.deepakbaliga.instasdk.callback.FeedsCallback;
 import com.deepakbaliga.instasdk.callback.MediaDetailsCallback;
 import com.deepakbaliga.instasdk.callback.UserCallback;
 import com.deepakbaliga.instasdk.entity.model.InstaError;
+import com.deepakbaliga.instasdk.entity.reponse.FeedResponse;
 import com.deepakbaliga.instasdk.entity.reponse.MediaReponse;
 import com.deepakbaliga.instasdk.entity.reponse.UserResponse;
 import com.deepakbaliga.instasdk.util.InstaConstants;
@@ -95,5 +98,41 @@ public class InstaMedia {
 
     }
 
+    /**
+     * Search for media in a given area. The default time span is set to 5 days
+     * Can return mix of image and video types.
+     *
+     * @param latitude  Latitude of the center search coordinate. If used, longitude is required.
+     * @param longitude Longitude of the center search coordinate. If used, latitude is required.
+     * @param distance  Default is 1km , max distance is 5km.
+     * @param callback
+     */
+    public void searchMediaByLocation(double latitude, double longitude,
+                                      @IntRange(from = 1, to = 5) int distance, final FeedsCallback callback) {
 
+        final Call<FeedResponse> call = endpoint.searchMediaByLocation(latitude, longitude, distance * 1000, accessToken);
+
+        if (!NetworkUtility.isNetworkConnected(context))
+            callback.onFailure(noInternet());
+
+        else if (accessToken == null)
+            callback.onFailure(noAccessToken());
+
+        else
+            call.enqueue(new Callback<FeedResponse>() {
+                @Override
+                public void onResponse(Response<FeedResponse> response, Retrofit retrofit) {
+                    if (response.isSuccess())
+                        callback.onSuccess(response.body().getFeeds());
+                    else
+                        callback.onFailure(new InstaError(response.code(), response.raw().message()));
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+
+                }
+            });
+
+    }
 }
